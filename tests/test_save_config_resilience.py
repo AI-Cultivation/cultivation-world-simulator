551d2daabfdedc0852a038351585f47d2d8bc1c1
bridge <wangxin4qiao@163.com>
2026-08-07 01:17:04 +0800

    update: version to 4.1.0

--- tests/test_save_config_resilience.py
+++ tests/test_save_config_resilience.py
@@ -2,4 +2,4 @@ from src.utils.config import CONFIG
 
 
 def test_static_config_exposes_version_through_meta_section():
-    assert CONFIG.meta.version == "4.0.3"
+    assert CONFIG.meta.version == "4.1.0"
